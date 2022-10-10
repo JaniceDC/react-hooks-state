b34aaae8324ade12dccb76637d27b0ebb8a4a092
JaniceDC <janice.dipayan.chen@gmail.com>
2022-10-10 12:21:20 +1100

    reviewed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,44 @@
 import React, { useState } from "react";
 
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     setCount(count + 1);
+//   }
+
+//   return <button onClick={increment}>I have been clicked {count} times</button>;
+// }
+
+// export default Counter;
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     console.log(`before setState: ${count}`);
+
+//     setCount(count + 1);
+
+//     console.log(`after setState: ${count}`);
+//   }
+
+//   return <div onClick={increment}>{count}</div>;
+// }
+
 function Counter() {
   const [count, setCount] = useState(0);
 
+  // function increment() {
+  //   console.log(`before setState: ${count}`);
+  //   setCount(count + 1);
+  //   console.log(`after setState once: ${count}`);
+  //   setCount(count + 1);
+  //   console.log(`after setState twice: ${count}`);
+  // }
   function increment() {
-    setCount(count + 1);
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
   }
-
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <div onClick={increment}>{count}</div>;
 }
-
 export default Counter;
